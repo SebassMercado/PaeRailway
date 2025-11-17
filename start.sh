a2ddd171643febb8e8aa16b42e5d589c3ee97640
SebassMercado <sebassmercado97@gmail.com>
2025-11-17 13:56:23 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
 chmod +x apache-tomcat-9.0.112/bin/catalina.sh
 export JAVA_HOME=$JAVA_HOME
-export CATALINA_OPTS="-Dport.http=$PORT"
+export PATH=$JAVA_HOME/bin:$PATH
 apache-tomcat-9.0.112/bin/catalina.sh run
