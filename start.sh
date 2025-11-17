468fd9835a9aa50dac431c277089477f9471099a
SebassMercado <sebassmercado97@gmail.com>
2025-11-17 14:29:45 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Descomprimir WAR si es necesario
-mkdir -p /app/tomcat/webapps
-cp target/PaeRailway.war /app/tomcat/webapps/ROOT.war
+# Asegurarse de que el WAR se sirva como ROOT
+rm -rf apache-tomcat-9.0.112/webapps/ROOT
+cp apache-tomcat-9.0.112/webapps/Pae.war apache-tomcat-9.0.112/webapps/ROOT.war
+
+# Dar permisos de ejecución a scripts de Tomcat
+chmod +x apache-tomcat-9.0.112/bin/*.sh
 
 # Iniciar Tomcat
-cd apache-tomcat-9.0.112/bin
-chmod +x *.sh
-./catalina.sh run
+apache-tomcat-9.0.112/bin/catalina.sh run
